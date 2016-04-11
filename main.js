6fce3a57f6c697242fcaa73655a92d7b664cc5db
empecot <empecot@yahoo.com>
2016-04-11 16:04:18 -0400

    Added bg image and text

---
+++ main.js
@@ -0,0 +1,17 @@
+console.log( "hello, wrold" );
+
+
+$(".bg_img").fadeTo(0, 0);
+setTimeout(function(){ 
+        $(".bg_img").fadeTo(1000, 1)
+}, 300);
+
+ $("h1").fadeTo(0, 0);
+setTimeout(function(){ 
+        $("h1").fadeTo(850, 1)
+}, 1000);
+
+ $("h3").fadeTo(0, 0);
+setTimeout(function(){ 
+        $("h3").fadeTo(850, 1)
+}, 1800);
